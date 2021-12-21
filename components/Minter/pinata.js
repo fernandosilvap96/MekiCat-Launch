5a009986ceedc41a3a9d0c5a269e609c2e52c8da
fernandosilvap96 <fernando-p_@outlook.com>
2021-12-20 21:11:05 -0300

    pinata

---
+++ components/Minter/pinata.js
@@ -0,0 +1,58 @@
+//imports needed for this function
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+pinataSecretApiKey = '78401023905a82ced7d2ebc89eac6eacef05e293fdd982f1d0f40b474f4b508b';
+pinataApiKey = 'e0258d8806991d3540f2';
+
+export const pinFileToIPFS = (pinataApiKey, pinataSecretApiKey) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+
+    //we gather a local file for this example, but any valid readStream source will work here.
+    let data = new FormData();
+    data.append('file', fs.createReadStream('./yourfile.png'));
+
+    //You'll need to make sure that the metadata is in the form of a JSON object that's been convered to a string
+    //metadata is optional
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios
+        .post(url, data, {
+            maxBodyLength: 'Infinity', //this is needed to prevent axios from erroring out with large files
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: pinataApiKey,
+                pinata_secret_api_key: pinataSecretApiKey
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
